b3fab0251850737f796568e67f5386e7bb5206e2
Thomas Mellenthin <github@dysternis.de>
2022-10-28 21:46:02 +0200

    Initial Commit

---
+++ RubiksCubeTimer/RubiksCubeTimerApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2022 Thomas Mellenthin (privat). All rights reserved.
+
+import SwiftUI
+
+@main
+struct RubiksCubeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
